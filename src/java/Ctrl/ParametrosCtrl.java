07c459cc50e72f2b0d97e11138cf2f602d83e263
Lud1024 <165453332+Lud1024@users.noreply.github.com>
2024-05-06 09:04:40 -0600

    Modificacion Parametros

--- src/java/Ctrl/ParametrosCtrl.java
+++ src/java/Ctrl/ParametrosCtrl.java
@@ -12,7 +12,6 @@ import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Combobox;
-import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 
 public class ParametrosCtrl extends GenericForwardComposer {
